26bd09937ca61880690ecc0968c4ff50ae47fbc1
JOvenOven <>
2023-02-23 11:14:44 -0600

    improved performance

--- src/App.js
+++ src/App.js
@@ -1,61 +1,36 @@
 import { useState } from 'react'
-import useFormulario from './hooks/useFormulario'
-import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
-import Button from './components/Button'
+import UserForm from './components/UserForm'
 
 function App() {
   const [usuarios, setUsuarios] = useState([])
-  const [formulario, handleChange, reset] = useFormulario({ 
-    name: '', 
-    email: '',
-    lastname: '',
-  })
 
-  const submit = e => {
-    e.preventDefault()
+  const submit = usuario => {
     setUsuarios([
       ...usuarios,
-      formulario,
+      usuario,
     ])
-    reset()
   }
 
+  console.log(usuarios);
   return(
-    <Container>
-      <Card>
-        <div style={{padding: 20}} >
-          <form onSubmit={submit}>
-            <Input 
-              label="Nombre"
-              name="name" 
-              value={formulario.name} 
-              onChange={handleChange} 
-            />
-            <Input 
-              label="Apellido"
-              name="lastname" 
-              value={formulario.lastname} 
-              onChange={handleChange} 
-            />
-            <Input 
-              label="Correo"
-              name="email" 
-              value={formulario.email} 
-              onChange={handleChange} 
-            />
-            <Button>Enviar</Button>
-          </form>
-        </div>
-      </Card>
-      <Card>
-        <ul>
-          {usuarios.map(x => 
-            <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>)}
-        </ul>
-      </Card>
-    </Container>
+    <div style={{ marginTop: '15%' }}>
+      <Container>
+        <Card>
+          <div style={{padding: 20}} >
+            <UserForm submit={submit} />
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map(x => 
+              <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>)}
+          </ul>
+        </Card>
+      </Container>
+    </div>
+    
   )
 }
 
